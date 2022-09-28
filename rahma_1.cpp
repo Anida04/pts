e399d733d0488e6b2e93783eab5957de2d62a902
Anida04 <111961923+Anida04@users.noreply.github.com>
2022-09-28 16:38:16 +0700

    Add files via upload

---
+++ rahma_1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+int main()
+{
+    cout << "Nama : Rahma Tri A"<<endl;
+    cout << "Kelas: 10 Rpl 1"<<endl;
+    cout << "Nilai: 95"<<endl;
+}
